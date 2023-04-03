d962fa3d15de9ed3576dfcdd4c9faf4eeac45641
Jackson_Wu <dengwu.wu@foxmail.com>
2023-04-03 16:39:25 +0800

    feat: init model

---
+++ AlgorithmWorld/model/model.py
@@ -0,0 +1,98 @@
+import datetime
+import uuid
+
+from sqlalchemy import (
+    create_engine,
+    Integer,
+    BigInteger,
+    String,
+    DateTime
+)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import sessionmaker
+
+from AlgorithmWorld.extensions import db
+
+# 基础类
+Base = declarative_base()
+
+# 创建引擎
+engine = create_engine(
+    "mysql+pymysql://tom:123@192.168.0.120:3306/db1?charset=utf8mb4",
+    # 超过链接池大小外最多创建的链接
+    max_overflow=0,
+    # 链接池大小
+    pool_size=5,
+    # 链接池中没有可用链接则最多等待的秒数，超过该秒数后报错
+    pool_timeout=10,
+    # 多久之后对链接池中的链接进行一次回收
+    pool_recycle=1,
+    # 查看原生语句（未格式化）
+    echo=True
+)
+
+# 绑定引擎
+Session = sessionmaker(bind=engine)
+# 创建数据库链接池，直接使用session即可为当前线程拿出一个链接对象conn
+# 内部会采用threading.local进行隔离
+session = scoped_session(Session)
+
+
+class User(Base):
+    """ 必须继承Base """
+    # 数据库中存储的表名
+    __tablename__ = "user"
+
+    userId = db.Column(BigInteger, primary_key=True, autoincrement=True)
+    username = db.Column(String(20))
+    password = db.Column(String(128))
+    isRoot = db.Column(Integer, default=0)
+    updateTime = db.Column(DateTime, default=datetime.datetime.now())
+    createTime = db.Column(DateTime, default=datetime.datetime.now())
+    version = db.Column(BigInteger)
+    deleted = db.Column(Integer, default=0)
+    __mapper_args__ = {
+        'version_id_col': version,
+    }
+
+
+class User_Record(Base):
+    """ 必须继承Base """
+    # 数据库中存储的表名
+    __tablename__ = "user_record"
+    recordId = db.Column(BigInteger, primary_key=True, autoincrement=True)
+    userId = db.Column(BigInteger)
+    AlgorithmPkgId = db.Column(BigInteger)
+    updateTime = db.Column(DateTime, default=datetime.datetime.now())
+    createTime = db.Column(DateTime, default=datetime.datetime.now())
+    version = db.Column(BigInteger)
+    deleted = db.Column(Integer, default=0)
+    __mapper_args__ = {
+        'version_id_col': version,
+    }
+
+
+class AlgorithmPkg(Base):
+    """ 必须继承Base """
+    # 数据库中存储的表名
+    __tablename__ = "algorithm_pkg_id"
+    name = db.Column(String(150))
+    userId = db.Column(BigInteger)
+    algorithmPkgId = db.Column(BigInteger)
+    tag = db.Column(String(30))
+    location = db.Column(String(150))
+    updateTime = db.Column(DateTime, default=datetime.datetime.now())
+    createTime = db.Column(DateTime, default=datetime.datetime.now())
+    version = db.Column(BigInteger)
+    deleted = db.Column(Integer, default=0)
+    __mapper_args__ = {
+        'version_id_col': version,
+    }
+
+
+if __name__ == "__main__":
+    # 删除表
+    Base.metadata.drop_all(engine)
+    # 创建表
+    Base.metadata.create_all(engine)
