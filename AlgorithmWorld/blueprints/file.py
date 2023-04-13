b5403bcfc90be5e55748feeb30943f2d751deee5
Jackson_Wu <dengwu.wu@foxmail.com>
2023-04-13 23:35:40 +0800

    feat: img upload finish

--- AlgorithmWorld/blueprints/file.py
+++ AlgorithmWorld/blueprints/file.py
@@ -1,15 +1,17 @@
 from flask import Blueprint, request
+
+from AlgorithmWorld.utils.jwtUtils import login_required
 from AlgorithmWorld.utils.upload import upload_files
 from AlgorithmWorld.config.config import DevelopmentConfig
 
 file_bp = Blueprint('file', __name__)
 
 
-@file_bp.route('/upload', methods=['POST'])
-def upload():
+@file_bp.route('/uploadImg', methods=['POST'])
+@login_required
+def uploadImg():
     file_path = upload_files(base_path=DevelopmentConfig.UPLOAD_FOLDER)
     if file_path is None:
         return {"code": 200, "message": "上传失败"}
 
     return {"code": 200, "message": "上传成功", "file_path": file_path}
-
