56aa70ffa32512d1c653557c4dcca015e04f07ce
Jackson_Wu <dengwu.wu@foxmail.com>
2023-04-17 18:05:24 +0800

    feat: add changeUserInfo

--- AlgorithmWorld/blueprints/admin.py
+++ AlgorithmWorld/blueprints/admin.py
@@ -1,16 +1,8 @@
-import json
-
-from flask import Blueprint
-
-from AlgorithmWorld.extensions import db
-from AlgorithmWorld.model.model import User
-from AlgorithmWorld.utils.jwtUtils import root_required, login_required
 from flask import Blueprint, request
 
-from AlgorithmWorld.dao import userDao
 from AlgorithmWorld.extensions import db
 from AlgorithmWorld.model.model import User
-from AlgorithmWorld.utils.jwtUtils import create_token, login_required, root_required, get_jwt_payload
+from AlgorithmWorld.utils.jwtUtils import login_required, root_required
 from AlgorithmWorld.utils.md5 import password_md5
 from AlgorithmWorld.utils.pack import packUser
 
@@ -47,3 +39,22 @@ def addUser():
 
     del data['password']
     return {"code": 200, "data": data}
+
+
+@admin_bp.route('/changeUserInfo', methods=['PUT'])
+@login_required
+@root_required
+def updateUser():
+    data = request.json
+    user = packUser(data)
+    query_user = db.session.query(User).filter(User.userId == user.userId).first()
+    if query_user is None: return {"code": 200, "message": "没有此用户"}
+    db.session.query(User).filter(User.userId == user.userId).update({
+        User.email: user.email,
+        User.tel: user.tel,
+        User.imageUrl: user.imageUrl,
+        User.isRoot: user.isRoot
+    })
+
+    db.session.commit()
+    return {"code": 200, "message": "更新用户成功"}
--- AlgorithmWorld/utils/pack.py
+++ AlgorithmWorld/utils/pack.py
@@ -3,10 +3,12 @@ from AlgorithmWorld.model.model import User
 
 def packUser(data):
     user = User()
-    user.username = data['username']
-    user.isRoot = data['isRoot']
-    user.password = data['password']
-    user.imageUrl = data['imageUrl']
-    user.email = data['email']
-    user.tel = data['tel']
+    keys = data.keys()
+    if 'userId' in keys: user.userId = data['userId']
+    if 'username' in keys: user.username = data['username']
+    if 'isRoot' in keys: user.isRoot = data['isRoot']
+    if 'password' in keys:  user.password = data['password']
+    if 'imageUrl' in keys:  user.imageUrl = data['imageUrl']
+    if 'email' in keys: user.email = data['email']
+    if 'tel' in keys:  user.tel = data['tel']
     return user
